759f58e4a86ef6e3d881a25765c1b7c00ad7c76f
hyuntall <hyunchul1998@naver.com>
2022-10-05 22:11:39 +0900

    best student'

---
+++ 3197.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+#define MAX 1500;
+
+int r, c;
--- BestStudent.cpp
+++ BestStudent.cpp
@@ -4,45 +4,47 @@
 // 학교는 그 학생에게 수상을 할 계획이다.
 // n일 연속 및 해당 쿼리에 대해 최고의 학생 목록이 주어지면
 // 해당 기간동안 가장 빈번히 선택될 최고의 학생을 찾는 프로그램을 작성한다.
-
+// 첫 줄에 표준 입력으로 일 수 n과, 학생의 수 m이 주어지고,
+// 둘째 줄에 일별로 우수한 학생의 번호를 기록한 수들의 쿼리가 주어진다.
 #include <iostream>
-#include <vector>
 #include <map>
 #include <algorithm>
+#include <vector>
+
+#define pp pair<int, int>
+
 using namespace std;
+
+bool cmp(const pp& a, const pp& b) {
+	if (a.second == b.second) return a.first > b.first;
+	return a.second < b.second;
+}
+
 int	main()
 {
 	int n, m;
 	int a, b;
-	vector <pair <int, int> > queries;
-
-	cin >> n >> m;
-	int	days[n];
-	for (int i = 0; i < n; i++)
-		cin >> days[i];
-
-	for (int i = 0; i < m; i++)
-	{
-		cin >> a >> b;
-		queries.push_back(make_pair(a, b));
-	}
+	int querie[1000001];
 	
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+		cin >> querie[i];
 	for (int i = 0; i < m; i++)
 	{
-		map <int, int> dict;
-		
-		for (int j = queries[i].first; j < queries[i].second; i++)
+		map<int, int> dict;
+		cin >> a >> b;
+		for (int i = a-1; i < b; i++)
 		{
-			if (dict.find(days[j]) != dict.end())
-				dict[days[j]] += 1;
+			if (dict[querie[i]])
+				dict[querie[i]] += 1;
 			else
-				dict[days[j]] = 1;
+				dict[querie[i]] = 1;
+			cout << querie[i]<< "값" << dict[querie[i]] << "\n";
 		}
-		vector < pair <int, int> > result;
-		for (int j = 0; j < dict.size(); i++)
-		{
-			result[j] = make_pair(dict[j].first, dict[j].second);
-		}
+		vector<pp> vec(dict.begin(), dict.end());
+		sort(vec.begin(), vec.end(), [](pp a, pp b){
+			return a.second > b.second;
+		});
+		cout << vec[0].first;
 	}
-	return 0;
 }
